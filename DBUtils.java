e5139bfc4a28b603c1c1a846cdf4cb84c0fb37df
sambijit <sambijitpattanaik2@gmail.com>
2024-01-12 18:26:49 +0530

    added stage

--- DBUtils.java
+++ DBUtils.java
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 Hello this is the master branch code..
+=======
+this line updating in dev branch..
+>>>>>>> devopment
 Hello this file contains Database rlated code...
 having some utiities for connection
