b99e0bfd370e806b35d9cd77d269c80662971aa5
sambijit <sambijitpattanaik2@gmail.com>
2024-01-17 05:50:44 +0530

    updated in dev branch

--- DBUtils.java
+++ DBUtils.java
@@ -1,5 +1,4 @@
-This is the master branch code..
-This line updating in dev branch
+Hello this is the master branch code..
 This line updating in dev branch..
 Hello this file contains database related code...
 having some utilities for connection
